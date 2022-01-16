b988280a6ac30e2437b24ef0a05de0e770ddd153
Paresh Kanani <paresh.kanani@solutionanalysts.com>
2022-01-16 20:27:03 +0530

    Added test cases

---
+++ CocoExamplePod/SwiftyLib.swift
@@ -0,0 +1,13 @@
+public final class SwiftyLib {
+
+    let name = "SwiftyLib"
+    
+    public func add(a: Int, b: Int) -> Int {
+        return a + b
+    }
+    
+    public func sub(a: Int, b: Int) -> Int {
+        return a - b
+    }
+    
+}
