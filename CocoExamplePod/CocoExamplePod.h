16fda0c4bebbd5a65f2dca5ebf02ad563e4be4b1
Paresh Kanani <paresh.kanani@solutionanalysts.com>
2022-01-16 20:14:50 +0530

    Initial project setup

---
+++ CocoExamplePod/CocoExamplePod.h
@@ -0,0 +1,18 @@
+//
+//  CocoExamplePod.h
+//  CocoExamplePod
+//
+//  Created by Paresh Kanani on 16/01/22.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for CocoExamplePod.
+FOUNDATION_EXPORT double CocoExamplePodVersionNumber;
+
+//! Project version string for CocoExamplePod.
+FOUNDATION_EXPORT const unsigned char CocoExamplePodVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <CocoExamplePod/PublicHeader.h>
+
+
